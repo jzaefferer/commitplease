424f3c7eb56ca690264e1f996418a54b82e3d9be
Rui Marinho <rpm@seegno.com>
2014-06-26 15:52:32 +0100

    Validate: Add check for empty commit subject line
    This also fixes the subject-after-component check, which ignored the
    component option incorrectly.
    
    Closes #20

--- test.js
+++ test.js
@@ -80,6 +80,17 @@ var valid = [
 
 var invalid = [
 	{
+		msg: "",
+		expected: [ "First line (subject) must not be empty" ],
+		options: {
+			component: false
+		}
+	},
+	{
+		msg: "",
+		expected: [ "First line (subject) must not be empty", "First line (subject) must indicate the component", "First line (subject) must have a message after the component" ]
+	},
+	{
 		msg: "Component: short message but actually a little bit over default character limit",
 		expected: [ "First line (subject) must be no longer than 72 characters" ]
 	},
