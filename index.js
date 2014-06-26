e03a7dc48280f15d611c9b0be13b53fe57485ead
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-06-26 15:40:16 +0100

    Index: Add support for custom options via package.json
    Looks for a commitplease property in the package.json file. If found, it uses
    that to override any of the default options.
    
    Closes #21

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var fs = require( "fs" ),
+	path = require( "path" ),
+	root = path.resolve( __dirname, "../.." ),
 	chalk = require( "chalk" ),
-	validate = require( "./lib/validate" );
+	validate = require( "./lib/validate" ),
+	options = require( path.join( root, "package.json" ) ).commitplease || {};
+
 module.exports = function( messageFile ) {
 	var message = fs.readFileSync( messageFile ).toString();
-	var errors = validate( message );
+	var errors = validate( message, options );
 	if ( errors.length ) {
 		console.error( "Invalid commit message, please fix the following issues:\n" );
 		console.error( chalk.red( "- " + errors.join( "\n- " ) ) );
