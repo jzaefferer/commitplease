dbfaa8bf4b337771117d5a8cf41fa554820ad48d
Alexander Lisianoi <all3fox@gmail.com>
2017-02-15 16:48:32 +0100

    Use "ask forgiveness, not permission" in prepare-commit-msg-hook.js
    Use require('commitplease').getOptions() instead of storing getOptions

--- prepare-commit-msg-hook.js
+++ prepare-commit-msg-hook.js
@@ -2,17 +2,16 @@
 
 var fs = require('fs')
 
-var getOptions = require('commitplease').getOptions
-
-var options = getOptions()
+var options = require('commitplease').getOptions()
 
 var oldMessagePath = options.oldMessagePath
 var oldMessageSeconds = options.oldMessageSeconds
 
-if (fs.existsSync(oldMessagePath)) {
-  // There is an old message that was previously rejected by us
+try {
+  // There may be an old message that was previously rejected by us
   // Suggest it to the user so they do not have to start from scratch
 
+  // Will throw ENOENT if no such file, ask forgiveness not permission
   var mtime = new Date(fs.statSync(oldMessagePath).mtime)
 
   // Date.now() - mtime.getTime() is milliseconds, convert to seconds
@@ -21,4 +20,10 @@ if (fs.existsSync(oldMessagePath)) {
   }
 
   fs.unlinkSync(oldMessagePath)
+} catch (err) {
+  if (!/ENOENT/.test(err.message)) {
+    throw err
+  }
+
+  // there is no old message to reuse, swallow the exception
 }
